7578df47870eb25e0cb2c3a76bb890fc4c0ce882
Lucas Limão Debia de Oliveira <lucas.limao.debia@gmail.com>
2020-03-18 12:56:47 -0300

    removi estrutura repository

--- src/repository/index.js
+++
@@ -1,6 +0,0 @@
-const repository = require('node-mongoose-repository')
-const schema = require('./schema')
-
-const getTableName = modelName => `musii-${modelName}`.toLowerCase()
-
-module.exports = () => repository.map('User', getTableName('User'), schema)
